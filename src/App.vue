f613b803ef967781385d6669b0bd332fcb8b60b8
Cenk Bülbül <95653055+cenkbulbull@users.noreply.github.com>
2024-02-14 22:50:40 +0300

    project updated

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,11 @@
 <template>
+  <AppHeader/>
+  <AppNavbar/>
   <router-view/>
+  <AppFooter/>
 </template>
+<script setup>
+import AppHeader from './components/AppHeader.vue'
+import AppNavbar from './components/AppNavbar.vue'
+import AppFooter from './components/AppFooter.vue'
+</script>
---
+++ src/views/About.vue
@@ -0,0 +1,83 @@
+<template>
+    <div class="site-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-12 text-left">
+            <div class="section-title mb-5">
+              <h2>Hakkımda</h2>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-lg-6">
+            <p>
+              <img src="images/big_img_1.jpg" alt="Image" class="img-fluid">
+            </p>
+          </div>
+          <div class="col-lg-6 pl-md-5">
+            <p>
+              18.06.2001 Zonguldak doğumluyum. Liseyi Zonguldak Erdemir Anadolu Lisesi'nde okudum. 2019 yılında Bülent Ecevit Üniversitesi Bilgisayar Mühendisliği bölümünü kazandım. 2023 yılında mezun oldum. Üniversite hayatımda web geliştirme alanına ilgi duydum ve bu alanda kendimi geliştirdim. Şuan Vue Js ile frontend tarafında çalışmaktayım.
+            </p>
+            <ul class="ul-check list-unstyled success mt-2">
+              <li>Html</li>
+              <li>Css</li>
+              <li>Javascript</li>
+              <li>Jquery</li>
+              <li>Vue</li>
+              <li>Bootstrap</li>
+              <li>PrimeVue</li>
+              <li>Vuex</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+    <div class="site-section">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="row">
+              <div class="col-12">
+                <div class="section-title">
+                  <h2>Çalışmalar</h2>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-6">
+                <div class="post-entry-1">
+                  <a href="githublink"><img src="images/img_h_1.jpg" alt="Image" class="img-fluid"></a>
+                  <h2><a href="githublink">Proje İsmi</a></h2>
+                  <p>Proje açıklaması</p>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="post-entry-1">
+                  <a href="githublink"><img src="images/img_h_1.jpg" alt="Image" class="img-fluid"></a>
+                  <h2><a href="githublink">Proje İsmi</a></h2>
+                  <p>Proje açıklaması</p>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="post-entry-1">
+                  <a href="githublink"><img src="images/img_h_1.jpg" alt="Image" class="img-fluid"></a>
+                  <h2><a href="githublink">Proje İsmi</a></h2>
+                  <p>Proje açıklaması</p>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="post-entry-1">
+                  <a href="githublink"><img src="images/img_h_1.jpg" alt="Image" class="img-fluid"></a>
+                  <h2><a href="githublink">Proje İsmi</a></h2>
+                  <p>Proje açıklaması</p>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+</template>
